7274e90a9a88d4f98741a33171bc68339f13eedb
SK ABDUL FAHEMID <53863051+FAHEMID03@users.noreply.github.com>
2024-06-25 11:31:18 +0530

    Create sequencer.sv
    The sequencer is a mediator who establishes a connection between sequence and driver.

---
+++ sequencer.sv
@@ -0,0 +1,11 @@
+class sequencer extends uvm_sequencer;
+ `uvm_component_utils(seqcr)
+
+  function new(string name="seqcr",uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+  endfunction
+endclass
